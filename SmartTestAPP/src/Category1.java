af81ca3c2e9b7db0d20eabad15d334625943aa6d
Julianne Nuetzel <juli.nuetzel@gmail.com>
2017-05-07 17:48:36 -0400

    added Learning Outcome Classes

---
+++ SmartTestAPP/src/Category1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author csc190
+ */
+public class Category1 implements java.io.Serializable{
+    //keeps array of all of their category 2 names
+    protected String name; //learning outcome name
+    protected Category2 [] arrCat2; 
+    public Category1 (String inpN, Category2 [] inpArrCat2){
+        this.name = inpN;
+        this.arrCat2 = inpArrCat2;
+    }
+      
+}
